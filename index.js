e58b2028da70b7f1d70a889b222a17348c766e79
pickuse <pickuse2013@gmail.com>
2023-05-22 15:59:00 +0800

    用正確的方式提取路徑

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const remainingFilenameLength = require('./remainingFilenameLength.js');
 
-const path = 'C:\\Users\\picku\\Documents\\01_53g給選手_v2\\01_53g給選手_v2\\a';
+// const path = 'C:\\Users\\picku\\Documents\\01_53g給選手_v2\\01_53g給選手_v2\\a';
+const path = '/Users/pickuse/Documents/sssss';
 const remainingLength = remainingFilenameLength(path);
 
 console.log(`剩餘可用檔名字元長度: ${remainingLength}`);
 
 // create file at path
-// windows file length limit is 255
-// windows full path length limit is 260
+
 const fs = require('fs');
-const filename = 'a'.repeat(remainingLength + 41 + 2 + 1) + '.txt';
+const filename = 'a'.repeat(255 - 4) + '.txt';
 const content = 'Hello World!';
-const fullPath = `${path}\\${filename}`;
+// const fullPath = `${path}\\${filename}`;
+const fullPath = `${path}/${filename}`;
 
 fs.writeFile(fullPath, content, (err) => {
     if (err) {
--- remainingFilenameLength.js
+++ remainingFilenameLength.js
@@ -1,19 +1,28 @@
 const os = require('os');
+const path = require('path');
 
-function remainingFilenameLength(path) {
+function remainingFilenameLength(inputPath) {
     let maxLength;
+
+    // windows file length limit is 255
+    // windows full path length limit is 260
+
+    // mac file length limit is 255
+    // mac full path length limit is 1024
+
+    const dirPath = path.dirname(inputPath);
 
     // 根據作業系統設定最大路徑長度限制
     if (os.platform() === 'win32') {
         maxLength = 260;
     }
 
-    if (os.platform() === 'darwin') { 
+    if (os.platform() === 'darwin') {
         maxLength = 1024;
     }
 
     // 計算剩餘可用檔名字元長度
-    const remainingLength = maxLength - path.length;
+    const remainingLength = maxLength - dirPath.length;
 
     return remainingLength;
 }
