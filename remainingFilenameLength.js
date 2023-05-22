ecc00ecc1fadcf8b3c53086320eed384fd946158
pickuse <pickuse2013@gmail.com>
2023-05-22 16:08:44 +0800

    更新(remainingFilenameLength.js)：新增最大檔名長度為255的限制，避免超過檔名長度限制 修正(remainingFilenameLength.js)：回傳值現在會考慮最大檔名長度的限制

--- remainingFilenameLength.js
+++ remainingFilenameLength.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 function remainingFilenameLength(inputPath) {
     let maxLength;
+    let maxFilenameLength = 255;
 
     // windows file length limit is 255
     // windows full path length limit is 260
@@ -23,8 +24,8 @@ function remainingFilenameLength(inputPath) {
 
     // 計算剩餘可用檔名字元長度
     const remainingLength = maxLength - dirPath.length;
-
-    return remainingLength;
+    
+    return Math.min(remainingLength, maxFilenameLength);
 }
 
 module.exports = remainingFilenameLength;
